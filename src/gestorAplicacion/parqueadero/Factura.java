0d3bd035c4408ea0cf3095f9f086821fe8ae477c
Sabelsl <141966602+Sabelsl@users.noreply.github.com>
2023-10-03 18:40:06 -0500

    clase cliente

--- src/gestorAplicacion/parqueadero/Factura.java
+++ src/gestorAplicacion/parqueadero/Factura.java
@@ -1,5 +1,19 @@
 package gestorAplicacion.parqueadero;
 
 public class Factura {
+	private double totalCobro;
+	
+	public Factura(double cobro) {
+		this.setTotalCobro(cobro);
+		
+	}
 
+	public double getTotalCobro() {
+		return totalCobro;
+	}
+
+	public void setTotalCobro(double totalCobro) {
+		this.totalCobro = totalCobro;
+	}
+	
 }
--- src/gestorAplicacion/personas/Cliente.java
+++ src/gestorAplicacion/personas/Cliente.java
@@ -1,5 +1,8 @@
 package gestorAplicacion.personas;
 
-public class Cliente {
+public class Cliente extends Persona{
 
+	public Cliente(String nombre, long cedula, String placa, String correo) {
+		super(nombre,cedula,placa,correo);
+	}
 }

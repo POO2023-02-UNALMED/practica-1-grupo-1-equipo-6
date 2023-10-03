52b45da510ea401fa840b9750d463bc436799370
sebasz-a <jpabuena@unal.edu.co>
2023-10-02 21:54:04 -0500

    Clases parqueadero y moto
    Maqueta de las clases

--- src/gestorAplicacion/parqueadero/Parqueadero.java
+++ src/gestorAplicacion/parqueadero/Parqueadero.java
@@ -1,5 +1,40 @@
+//Sebastián
+
 package gestorAplicacion.parqueadero;
 
-public class Parqueadero {
+import java.util.ArrayList;
 
+/**
+ *Clase que instancia un parqueadero con servicio de parqueo, taller y concesionario.
+ */
+public class Parqueadero {
+	private int plazasTotales;
+	private int plazasDisponibles;
+	private double tarifa;
+	private ArrayList<Plaza> plazas = new ArrayList<Plaza>();
+	
+	public Parqueadero() {
+		
+	}
+	
+	//Getters and Setters
+	public int getPlazasTotales() {
+		return this.plazasTotales;
+	}
+	public void setPlazasTotales(int plazasTotales){
+		this.plazasTotales = plazasTotales;
+	}
+	public int getPlazasDisponibles() {
+		return this.plazasDisponibles;
+	}
+	public void setPlazasDisponibles(int plazasDisponibles) {
+		this.plazasDisponibles = plazasDisponibles;
+	}
+	public double getTarifa() {
+		return this.tarifa;
+	}
+	public void setTarifa(double tarifa) {
+		this.tarifa = tarifa;
+	}
+	
 }
---
+++ src/gestorAplicacion/vehiculos/Moto.java
@@ -0,0 +1,10 @@
+//Sebastián
+
+package gestorAplicacion.vehiculos;
+
+/**
+ * Clase que instancia una Moto.
+ */
+public class Moto extends Vehiculo{
+	
+}

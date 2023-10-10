0d672027ab1b3cc346ff2f27f4ce4e9bbfd9ae8f
alariaso <141962262+alariaso@users.noreply.github.com>
2023-10-10 18:14:17 -0500

    documentacion Consola y cambios en como se imprimen instrucciones

--- src/uiMain/Consola.java
+++ src/uiMain/Consola.java
@@ -5,7 +5,16 @@ package uiMain;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * Esta es una clase con diferentes métodos para interactuar con el usuario
+ * a través de la interfaz de consola, ya sea para imprimir mensajes o errores,
+ * para pedirle al usuario que ingrese valores o para que elija entre una lista
+ * de opciones, etc.
+ */
 public class Consola {
+	/**
+	 * Imprime un error en consola, mostrando su mensaje y si existe, el mensaje de su causa.
+	 */
 	public static void imprimirError(Exception error) {
 		System.out.println("Error: " + error.getMessage());
 		Throwable causa = error.getCause();
@@ -14,18 +23,32 @@ public class Consola {
 		}
 	}
 
+	/**
+	 * Imprime el mensaje y espera que el usuario ingrese un entero, retornándolo.
+	 */
 	public static int pedirEntero(String mensaje) {
-		System.out.print(mensaje + ": ");
+		imprimirMensaje(mensaje);
 		return scanner().nextInt();
 	}
 
+	/**
+	 * Imprime el mensaje y espera que el usuario ingrese un número long, retornándolo.
+	 */
 	public static long pedirLong(String mensaje) {
-		System.out.print(mensaje + ": ");
+		imprimirMensaje(mensaje);
 		return scanner().nextLong();
 	}
 
+	/**
+	 * Imprime el mensaje con un (si/no) y espera que el usuario ingrese "si" o "no".
+	 * Retorna un boolean: true si el usuario ingresa "si", false si ingresa "no".
+	 *
+	 * Sólo verifica la primera letra de lo que ingrese el usuario, sin importar si está
+	 * en mayúsculas o minúsculas. Por ejemplo, "s", "S", "Sxyz", "sabc" son todos considerados
+	 * como un "si"; "n", "N", "N...", "nao" son todos considerados como un "no".
+	 */
 	public static boolean pedirBoolean(String mensaje) {
-		System.out.print(mensaje + " (si/no): ");
+		imprimirMensaje(mensaje, "(si/no)");
 		char eleccion = scanner().nextLine().toLowerCase().charAt(0);
 		if (eleccion == 's') {
 			return true;
@@ -37,29 +60,44 @@ public class Consola {
 		}
 	}
 
+	/**
+	 * Imprime el mensaje y espera que el usuario ingrese una línea, retornándola
+	 * sin incluir separadores de línea.
+	 */
 	public static String pedirString(String mensaje) {
-		System.out.print(mensaje + ": ");
+		imprimirMensaje(mensaje);
 		return scanner().nextLine();
 	}
 
 	/**
 	 * Recibe el mensaje y una lista de opciones, devuelve el índice de la opción escogida.
-	 * Este metodo imprime una lista de opciones en pantalla y espera que se escoja una,
-	 * verifica que sea una opción válida y la retorna. Si no es válida entonces repite la
-	 * pregunta.
+	 * Este metodo imprime un mensaje junto con una lista de opciones en pantalla y espera
+	 * que el usuario escoja una, luego verifica que ésta sea una opción válida y la retorna.
+	 * Si no es válida entonces repite la pregunta.
 	 */
 	public static int pedirEleccion(String mensaje, List<String> opciones) {
 		System.out.println(mensaje);
 		for (int i = 0; i < opciones.size(); i++) {
 			System.out.printf("%d. %s%n", i+1, opciones.get(i));
 		}
-		System.out.print("Escoja una opción: ");
+		imprimirMensaje("Escoja una opción");
 		int eleccion = scanner().nextInt();
 		if (eleccion <= 0 || eleccion > opciones.size()) {
 			System.out.println("Opción inválida");
 			return pedirEleccion(mensaje, opciones);
 		}
 		return eleccion - 1;
+	}
+
+	/**
+	 * Imprime un mensaje precedido por 
+	 */
+	private static void imprimirMensaje(String mensaje) {
+		System.out.println("> " + mensaje + ": ");
+	}
+
+	private static void imprimirMensaje(String mensaje, String extra) {
+		imprimirMensaje(mensaje + " " + extra);
 	}
 
 	private static Scanner scanner() {

03070fa060de54907a611505ff22f039a23c21c3
KatherinePuentes <141962369+KatherinePuentes@users.noreply.github.com>
2023-10-21 02:45:52 +0000

    vender carro

--- src/uiMain/VenderCarro.java
+++ src/uiMain/VenderCarro.java
@@ -1,8 +1,30 @@
 package uiMain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import gestorAplicacion.personas.Cliente;
+import gestorAplicacion.vehiculos.Vehiculo;
+import gestorAplicacion.vehiculos.Carro;
+import gestorAplicacion.personas.Empleado;
+
+
 public class VenderCarro extends Funcionalidad {
 	@Override
 	public void ejecutar() {
+		System.out.print("Vender un carro");
+	}
 
+	long cedula= Consola.pedirLong("Ingrese cédula");
+
+	Cliente cliente= buscarORegistrarCliente(cedula);
+	if (Cliente==null){
+		return;
+	}
+
+	Vehiculo vehiculo= escogerVehiculo(cliente);
+	if (vehiculo== null){
+		return;
 	}
 }

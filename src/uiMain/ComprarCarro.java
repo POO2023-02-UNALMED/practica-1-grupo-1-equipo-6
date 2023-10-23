6b0b7a07897830c95bc83e67ffe9016a995faa36
SofiaGL082 <sogiraldo@unal.edu.co>
2023-10-23 00:25:57 -0500

    Avance comprarCarro

--- src/uiMain/ComprarCarro.java
+++ src/uiMain/ComprarCarro.java
@@ -11,6 +11,8 @@ import gestorAplicacion.personas.Empleado;
 import gestorAplicacion.vehiculos.Vehiculo;
 import gestorAplicacion.vehiculos.Carro;
 import gestorAplicacion.vehiculos.MarcasCarro;
+import gestorAplicacion.parqueadero.Producto;
+import gestorAplicacion.parqueadero.Almacen;
 
 //Permite que el cliente venda un carro al parqueadero
 public class ComprarCarro extends Funcionalidad {
@@ -47,8 +49,11 @@ public class ComprarCarro extends Funcionalidad {
 		
 		System.out.printf("Hola, mi nombre es %s y voy a atenderlo.%n", vendedor.getNombre());
 		
+		//Se crea el precio máximo para esa marca de carro
+		long precioMaximo = precioMaximo(vehiculo, vendedor);
+		
 		//Se le pide el precio por el que desea vender el carro
-		long precioCliente = precioCarro(vehiculo, vendedor);
+		long precioCliente = precioCarro(precioMaximo);
 		
 		System.out.println("Su carro será revisado en el taller y se le dará una oferta de compra.");
 		
@@ -63,7 +68,32 @@ public class ComprarCarro extends Funcionalidad {
 		
 		System.out.printf("Hola, mi nombre es %s y voy a atenderlo.%n", mecanicoVenta.getNombre());
 		
-		revisionTaller(vehiculo);
+		//Se crea la lista de productos que necesitan reparacion
+		List<Producto> productosMalos = mecanicoVenta.revisarVehiculo(vehiculo);
+		
+		//Se crea la cotizacion del parqueadero
+		long precioParqueadero = cotizacionParqueadero(productosMalos, mecanicoVenta, precioMaximo, cliente);
+		
+		//Se comparan los precios del cliente y el parqueadero y se deja como precio final el menor
+		long precioFinal = precioFinal(precioCliente, precioParqueadero);
+		
+		//Se crea una lista con todos los carros que tengan menor o igual precio al precio final
+		List<Carro> carrosDisponibles = new ArrayList<>(vendedor.getVehiculosVenta().stream().filter(carro -> carro.getPrecioVenta()<=precioFinal).collect(Collectors.toList()));
+		
+		//Se le permite al usuario escoger ente la venta del carro por dinero o intercabio por otro carro.
+		List<String> opcionesVenta = new ArrayList<>();
+		opcionesVenta.add("Vender el vehiculo por " + precioFinal);
+		opcionesVenta.add("Intercambiar su carro por uno disponible para la venta en el rango de precio.");
+		int opcionVenta = Consola.pedirEleccion("Su carro ha sido revisado en el taller, y podrá escoger entre las siguientes ofertas: ", opcionesVenta);
+		
+		if (opcionVenta == 1) {
+			ventaPorDinero(cliente, vehiculo, vendedor, mecanicoVenta, precioFinal, productosMalos);
+			System.out.println("Se ha generado su factura y ha finalizado la venta de vehiculo. ¡Adios!");
+			return;
+		} else {
+			
+		}
+		
 		
 		
 	}
@@ -129,8 +159,19 @@ public class ComprarCarro extends Funcionalidad {
 		}
 	}
 	
-	private long precioCarro(Vehiculo vehiculo, Empleado vendedor) {
+	private long precioCarro(long precioMaximo) {
 		long precioCliente = Consola.pedirLong("Escriba el precio por el que desea vender su vehículo");
+		if (precioCliente<0) {
+			System.out.println("Ese precio no es válido");
+			return precioCarro(precioMaximo); //se vuelve a pedir el precio del carro
+		} else if (precioCliente>precioMaximo) {
+			System.out.println("El precio escogido no es aceptado por el administrador");//Revisar mensaje
+			return precioCarro(precioMaximo);
+		} else {
+			return precioCliente;
+		}
+		}
+	private long precioMaximo(Vehiculo vehiculo, Empleado vendedor) {
 		String marca = ((Carro)vehiculo).getMarca();
 		long precioMaximo = 0;
 		if (marca.toUpperCase() == "RENAULT") {
@@ -144,20 +185,53 @@ public class ComprarCarro extends Funcionalidad {
 		}if (marca.toUpperCase() == "MAZDA") {
 			precioMaximo = vendedor.precioMaximoCarro(MarcasCarro.MAZDA);
 		}
-		if (precioCliente<0) {
-			System.out.println("Ese precio no es válido");
-			return precioCarro(vehiculo, vendedor); //se vuelve a pedir el precio del carro
-		} else if (precioCliente>precioMaximo) {
-			System.out.println("El precio escogido no es aceptado por el administrador");//Revisar mensaje
-			return precioCarro(vehiculo, vendedor);
-		} else {
-			return precioCliente;
+		return precioMaximo;
+	}
+	private long cotizacionParqueadero(List<Producto> productosMalos, Empleado mecanico, long precioMaximo, Cliente cliente) {
+		cliente.getFactura().agregarServicio("Revision general", 1);
+		for (Producto productoMalo: productosMalos) {
+			precioMaximo -= productoMalo.getPrecio();
 		}
-		}
-
+		mecanico.setServiciosRealizados(mecanico.getServiciosRealizados() + 1);
+		return precioMaximo;
+	}
 	
-	private void revisionTaller(Vehiculo vehiculo) {
-		
+	private long precioFinal(long precioCliente, long precioParqueadero) {
+		if (precioCliente<=precioParqueadero) {
+			return precioCliente;
+		} else {
+			return precioParqueadero;
+		}
 	}
 
+	private void ventaPorDinero(Cliente cliente, Vehiculo vehiculo, Empleado vendedor, Empleado mecanico, long precioFinal, List<Producto> productosMalos) {
+		//Se le asigna el precio de Venta al carro
+		((Carro)vehiculo).setPrecioVenta(precioFinal);
+		
+		//Se envia a reparar el carro
+		System.out.println("El vehiculo está siendo reparado en el taller");
+		Almacen almacen = parqueadero.getAlmacen();
+		for (Producto productoMalo: productosMalos) {
+			if (almacen.existeProducto(productoMalo.getTipo())) {
+				Producto productoBueno = almacen.conseguirProducto(productoMalo.getTipo());
+				mecanico.cambiar(productoMalo, productoBueno, vehiculo);
+			}
+		}
+		mecanico.setServiciosRealizados(mecanico.getServiciosRealizados() + 1);
+		
+		//Se añade el carro a la lista de carros disponibles para la venta
+		vendedor.agregarVehiculosVenta((Carro)vehiculo);
+		
+		//Se cambia el dueño del vehiculo por el parqueadero
+		vehiculo.setDueno(null);
+		
+		//Se genera la factura
+		cliente.getFactura().agregarServicio("Venta de carro", 1);
+		
+		vendedor.setServiciosRealizados(vendedor.getServiciosRealizados() + 1);
+	}
+	
+	private void cambioDeCarro(List<Carro> carrosDisponibles) {
+		
+	}
 }
